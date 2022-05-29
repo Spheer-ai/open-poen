435d4511ef0cbd12cf6e6e1b6101cfd6a2e652a5
Mark de Wijk <dewijkmark@gmail.com>
2022-05-29 18:03:56 +0200

    Added a project owner and three debit cards to the test for adding a project.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,26 +1,30 @@
-from app.models import Project
+from app.models import Project, User, DebitCard
 
 
 def test_add_project(db, client):
     response = client.post(
         "/",
         data={
-            "name": "Buurtborrel Oranjebuurt",
-            "description": "Buurtborrel / -BBQ voor de Oranjebuurt in Groningen.",
-            "purpose": "Creëren van saamhorigheid/gemeenschapsgevoel in de Oranjebuurt",
-            "target_audience": "40+ en jonger dan 18.",
+            "name": "Test Project",
+            "description": "A test.",
+            "purpose": "To test.",
+            "target_audience": "Developers.",
             "contains_subprojects": True,
-            "owner": "Jaap Koen Bijma",
-            "owner_email": "jaapkoenbijma@amsterdam.nl",
+            "owner": "Mark de Wijk",
+            "owner_email": "markdewijk@spheer.ai",
             "legal_entity": "Stichting",
-            "address_applicant": "De Dam 14, 9889 ST",
-            "registration_kvk": "1334998890",
-            "project_location": "Amsterdam",
-            "budget": 300,
+            "address_applicant": "Hereweg 14, Groningen",
+            "registration_kvk": "1111111111",
+            "project_location": "Groningen",
+            "budget": 10000,
+            "project_owners-0-email": "markdewijk@spheer.ai",
+            "card_numbers-0-card_number": "6731924673192111111",
+            "card_numbers-1-card_number": "6731924673192111112",
+            "card_numbers-2-card_number": "6731924673192111113",
         },
         follow_redirects=True,
     )
     assert response.status_code == 200
-    projects = Project.query.all()
-    assert len(projects) == 1
-    assert projects[0].name == "Buurtborrel Oranjebuurt"
+    assert Project.query.one()
+    assert User.query.one()
+    assert len(DebitCard.query.all()) == 3
