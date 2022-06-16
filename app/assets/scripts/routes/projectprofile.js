c2532736602cbe21ad615e023de34fde88f5af77
Mark de Wijk <dewijkmark@gmail.com>
2022-06-16 11:42:46 +0200

    Concept report buttons now link to a route instead of submitting a form.

--- app/assets/scripts/routes/projectprofile.js
+++ app/assets/scripts/routes/projectprofile.js
@@ -1,16 +1,25 @@
 export default {
     init() {
         const justify = "#justify-interactive-form"
-        const concept = "#concept-justify-interactive-form"
         $(justify).find(".funder-info").slice(1).hide();
         $(justify).find("[type=radio]").on("click", function () {
             $(justify).find(".funder-info").hide()
             $(justify).find("#funder-info-" + this.value).show()
+
+            const links = $(".report-link")
+            let pattern = new RegExp("funder/" + "\\d+")
+            links.attr("href", links.attr("href").replace(pattern, "funder/" + this.value))
         })
+
+        const concept = "#concept-justify-interactive-form"
         $(concept).find(".funder-info").slice(1).hide();
         $(concept).find("[type=radio]").on("click", function () {
             $(concept).find(".funder-info").hide()
             $(concept).find("#funder-info-" + this.value).show()
+
+            const links = $(".concept-report-link")
+            let pattern = new RegExp("funder/" + "\\d+")
+            links.attr("href", links.attr("href").replace(pattern, "funder/" + this.value))
         })
     },
     finalize() {
